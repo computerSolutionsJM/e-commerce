9fea9d683461d4e502a8e783f29faec801b4512e
jhonnyzb <jhonnyzb1@hotmail.com>
2020-12-11 15:49:32 -0500

    se agrega variables

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,11 @@ const GET_MESSAGES = gql`
 }
 `
 
+const Code = (p) => <code className={styles.inlineCode} {...p} />
+
 const Index = ({ pokemones }) => {
 
- console.log(pokemones)
+  console.log(pokemones)
 
   return (
     <>
@@ -40,6 +42,102 @@ const Index = ({ pokemones }) => {
       </Head>
       <ContainerMain>
         <h1>index</h1>
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <h1>Environment Variables with Next.js</h1>
+            <hr className={styles.hr} />
+            <p>
+              In the table below you'll see how{' '}
+              <a href="https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser">
+                environment variables can be exposed to the browser
+        </a>{' '}
+        with Next.js.
+      </p>
+            <p>
+              In general only <Code>.env.local</Code> or <Code>.env</Code> are needed
+        for this, but the table also features the usage of{' '}
+              <Code>.env.development</Code> and <Code>.env.production</Code>.
+      </p>
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>Variable Name</th>
+                  <th>Value</th>
+                  <th>Added By</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>NEXT_PUBLIC_ENV_VARIABLE</td>
+                  <td>{process.env.NEXT_PUBLIC_ENV_VARIABLE}</td>
+                  <td>
+                    <Code>.env</Code>
+                  </td>
+                </tr>
+                <tr>
+                  <td>NEXT_PUBLIC_ENV_LOCAL_VARIABLE</td>
+                  <td>{process.env.NEXT_PUBLIC_ENV_LOCAL_VARIABLE}</td>
+                  <td>
+                    <Code>.env.local</Code>
+                  </td>
+                </tr>
+                <tr>
+                  <td>NEXT_PUBLIC_DEVELOPMENT_ENV_VARIABLE</td>
+
+                  <td>{process.env.NEXT_PUBLIC_DEVELOPMENT_ENV_VARIABLE}</td>
+                  <td>
+                    <Code>.env.development</Code>
+                  </td>
+                </tr>
+                <tr>
+                  <td>NEXT_PUBLIC_PRODUCTION_ENV_VARIABLE</td>
+
+                  <td>{process.env.NEXT_PUBLIC_PRODUCTION_ENV_VARIABLE}</td>
+                  <td>
+                    <Code>.env.production</Code>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p>
+              <Code>.env.local</Code> is not added by the example, because it must be
+        ignored by git, but you can add it manually:
+      </p>
+            <pre>
+              <code>cp .env.local.example .env.local</code>
+            </pre>
+            <p>
+              Variables in <Code>.env.production</Code> won't be available if the app
+        is running in development:
+      </p>
+            <pre>
+              <code>npm run dev</code>
+            </pre>
+            <p>
+              Similarly, variables in <Code>.env.development</Code> won't be available
+        if the app is running on production:
+      </p>
+            <pre>
+              <code>npm run build && npm run start</code>
+            </pre>
+            <p>Once you run the app, you'll see logs like these in the terminal:</p>
+            <pre>
+              <code>
+                info - Loaded env from /home/user/../.env.local{'\n'}
+          info - Loaded env from /home/user/../.env.development{'\n'}
+          info - Loaded env from /home/user/../.env{'\n'}
+              </code>
+            </pre>
+            <p>
+              The order is important, the first loaded env will have a higher
+              priority.
+      </p>
+            <p>
+              <Code>.env</Code> will not overwrite any variables defined in{' '}
+              <Code>.env.local</Code> or <Code>.env.development</Code>.
+      </p>
+          </div>
+        </div>  
       </ContainerMain>
     </>
 
@@ -53,11 +151,13 @@ const Index = ({ pokemones }) => {
 
 export const getServerSideProps = wrapper.getServerSideProps(
   async ({ store, params }) => {
+    console.log('[Node.js only] ENV_VARIABLE:', process.env.ENV_VARIABLE)
+    console.log('[Node.js only] ENV_LOCAL_VARIABLE:', process.env.ENV_LOCAL_VARIABLE)
     const client = new GraphQLClient('https://backend-ecommerce-j.herokuapp.com/', { headers: {} })
     const { obtenerProductos } = await client.request(GET_MESSAGES)
     store.dispatch(obtenerPokemones(obtenerProductos))
   }
-) 
+)
 
 
 const mapStateToProps = (state) => ({
