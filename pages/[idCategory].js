3a2be764df61a28f376aa6a5ceab66cf4f9be4ae
Jhon Jairo Zabala Bohorquez <jhonnyzb1@hotmail.com>
2021-01-12 22:41:33 -0500

    se arregla error toaster undefined

--- pages/[idCategory].js
+++ pages/[idCategory].js
@@ -66,7 +66,6 @@ const Category = () => {
                               </Col>
                         </Row>
                   </ContainerMain>
-                  <Toaster />
             </>
       )
 }
